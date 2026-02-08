75ed4321110678a146cd8520c440a1c49586f7de
asad-au-ullah <asad.au.ullah@gmail.com>
2026-02-08 13:52:39 +0500

    Added API contracts for Breakfast

---
+++ BuberBreakfast.Contracts/UpsertBreakfastRequest.cs
@@ -0,0 +1,10 @@
+﻿//using System.Text;
+namespace BuberBreakfast.Contracts;
+
+public record UpsertBreakfastRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet);
